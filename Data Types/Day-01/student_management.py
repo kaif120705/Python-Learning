3ebfa734d9b73e58738c036592d33afb96a636d3
Mohammad Kaif Alam <mdkaifalam524@gmail.com>
2026-09-18 12:56:07 +0530

    Add Data Types Day 01

---
+++ Data Types/Day-01/student_management.py
@@ -0,0 +1,12 @@
+print("==" * 25)
+student_name="Mohammad Kaif Alam"
+_student_roll_no=33
+Cgpa=8.75
+fees_paid=True
+subject_name="Em,Mechanics,BEE,physics"
+print("student_name:",student_name,type(student_name))
+print("_student_roll_no:",_student_roll_no,type(_student_roll_no))
+print("Cgpa:",Cgpa,type(Cgpa))
+print("fees_paid:",fees_paid,type(fees_paid))
+print("subject_name:",subject_name,type(subject_name))
+print("==" * 25)
